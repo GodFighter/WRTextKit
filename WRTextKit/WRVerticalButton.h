70aa7360d6989c815dff5d52e2f2acfb5cba9023
God_Fighter <xianghui_ios@163.com>
2019-12-14 15:55:22 +0800

    添加蒙文按钮

---
+++ WRTextKit/WRVerticalButton.h
@@ -0,0 +1,17 @@
+//
+//  WRVerticalButton.h
+//  WRTextKitDemo
+//
+//  Created by xianghui-iMac on 2019/12/14.
+//  Copyright © 2019 项辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WRVerticalButton : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WRTextKit/WRVerticalButton.m
@@ -0,0 +1,21 @@
+//
+//  WRVerticalButton.m
+//  WRTextKitDemo
+//
+//  Created by xianghui-iMac on 2019/12/14.
+//  Copyright © 2019 项辉. All rights reserved.
+//
+
+#import "WRVerticalButton.h"
+
+@implementation WRVerticalButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
